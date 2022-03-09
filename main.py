2f12dadae994767f91ecf23900e58c89399ef3e8
samir <samir.mebrek@groupe-gema.com>
2022-03-09 14:03:01 +0100

    uncomment main

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-"""
-Created on Wed Mar  2 18:14:12 2022
-@author: SAMIR
-"""
-
-from AttackFunctions import DDosAttack
+import function
 
 
-site = input("IP: ")
-target = DDosAttack(site)
-target.begin()
+function.identite()
+function.age()
+function.cone()
+function.ispair()
+function.fibonacci()
+function.pgcd_ppcm()
+
+param1 = [1,2,3,4,5,6,7,8,9]
+param2 = [1,3,5,7,12,8,8,9,1]
+
+liste_commun = function.element_communs(param1, param2)
+print(liste_commun)
+
+function.Palindrome()
+liste = [1,2,5,4,8,6,1,2,7,8,5,2]
+resultaListe = function.doublon(liste)
+
+
